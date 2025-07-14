d94b9b0ea2d8eca33e1fd80c95401bd7df2da06a
Wang-Ande <Ande2049@163.com>
2025-07-14 11:33:20 +0800

    add：kegg富集分析本地部署

---
+++ ClusterProfiler/KEGG_local_download.R
@@ -0,0 +1,63 @@
+# Title: KEGG分析本地部署 (Human, hsa)
+# Author: [wangzihao]
+# Date: [2025-07-10]
+# Description:
+#   此脚本展示如何下载适用于Clusterprofiler包的KEGG数据格式(via `createKEGGdb`)。
+#   方便使用者本地进行KEGG富集分析，而不依赖KEGG API 路径。
+#   优点：适用于网络卡顿或者需要保证结果统一的分析。
+#   缺点：需要保证本地KEGG数据的更新。
+#
+# Dependencies:
+#   - createKEGGdb (from GitHub: YuLab-SMU/createKEGGdb)
+#   - clusterProfiler
+#   - KEGG.db (custom, locally installed)
+
+# pkgs ----       
+# pak("YuLab-SMU/createKEGGdb") #创建KEGG数据库的包的包
+library(createKEGGdb)
+#创建名称为KEGG.db_1.0.tar,gz的包。
+createKEGGdb::create_kegg_db("hsa")   # 需要保持更新！！！！！！！！！！！
+#安装这个包(默认的包的路径在当前工作目录，根据实际情况修改路径)
+install.packages("KEGG.db_1.0.tar.gz",repos=NULL,type="source")
+library(KEGG.db)
+library(clusterProfiler)
+library(pathview)
+library(org.Hs.eg.db)
+library(readxl)
+library(readr)
+library(dplyr)
+library(openxlsx)
+library(ggplot2)
+
+# DE_res input ----
+DP_result <- read.csv('./03_Result/DEP/OCI_AML2/Low_vs_Ctrl/result_DE.csv')
+
+# set P.Value ----
+GeneSymbol <- subset(DP_result, P.Value < 0.05)
+
+# set logFC ----
+cutoff <- 0.263                 # 对应fc约为1.2
+
+# 转换基因名 
+y <- GeneSymbol$Genes
+gene <- unlist(lapply(y,function(y) strsplit(as.character(y),";")[[1]][1]))
+GeneSymbol$gene <- gene
+
+# 设置数据库 
+GO_database <- 'org.Hs.eg.db'  # GO是org.Hs.eg.db数据库
+KEGG_database <- 'hsa'         # KEGG是hsa数据库
+
+# gene list ----
+down_genes <- subset(GeneSymbol, logFC < -cutoff)
+
+# gene ID转换 
+gene <- clusterProfiler::bitr(down_genes$gene, fromType = 'SYMBOL', toType = 'ENTREZID', OrgDb = GO_database)
+
+kk <- enrichKEGG(gene= gene$ENTREZI,
+                 keyType = "kegg",
+                 organism= 'hsa',
+                 pvalueCutoff = 0.05,
+                 pAdjustMethod = "BH",
+                 qvalueCutoff = 1,
+                 use_internal_data =T) # 当使用本地的KEGG.db数据库时，use_internal_data设置为T,使用在线数据库时设置为F.
+
